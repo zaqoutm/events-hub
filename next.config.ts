140c0087a641cbd885b098a838e900540696e441
“Mo <mo.zaqout@gmail.com>
2026-04-05 22:55:08 +0200

    connect to superbase

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utmtajtsswtcnwuwtvqi.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
   /* config options here */
 };
 
---
+++ src/lib/superbaseClients.ts
@@ -0,0 +1,8 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+const superbaseClient = createBrowserClient(url, key);
+
+export default superbaseClient;
